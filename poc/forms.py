fc230ed3c1a14acab1930d807acca9bad7e38b66
Poc-Django1 <81231863+Poc-Django1@users.noreply.github.com>
2021-03-30 12:30:45 +0530

    Delete poc directory

--- poc/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import subjectdetails
- 
-class MyForm(forms.ModelForm):
-  subject = forms.CharField(label='subject', max_length=30)
-  class Meta:
-    model = subjectdetails
-    fields = ['id', 'subject', ]
-    labels = {'id' : 'ID', 'subject' : 'Subject', 'Created' : 'Created_at', 'updated' : 'Updated_at'}
